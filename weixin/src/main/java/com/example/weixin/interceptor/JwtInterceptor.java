70a94ebecf207420729c02ca7ee7bfe73a4406ba
191250129 <191250129@smail.nju.edu.cn>
2021-05-26 19:42:16 +0800

    1

--- weixin/src/main/java/com/example/weixin/interceptor/JwtInterceptor.java
+++ weixin/src/main/java/com/example/weixin/interceptor/JwtInterceptor.java
@@ -22,16 +22,15 @@ public class JwtInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("nju-token");
         String longToken = request.getHeader("nju-long-token");
-        Integer uid = Integer.parseInt(request.getHeader("userId"));
-        Integer userType=Integer.parseInt(request.getHeader("userType"));
 //      如果短期token超时，长期token未超时 带上请求头
         if(token!=null&&longToken!=null){
             Integer userId = JwtUtil.verifyTokenAndGetUserId(token);
             Integer userId2 = JwtUtil.verifyTokenAndGetUserId(longToken);
+            Integer userType2=JwtUtil.verifyTokenAndGetUserType(longToken);
             if(userId==null&&userId2!=null){
                 //短token时长20min 长token时长60min
-                response.setHeader("nju-token",JwtUtil.createToken(uid,userType,shortTokenTime));
-                response.setHeader("nju-long-token",JwtUtil.createToken(uid,userType,longTokenTime));
+                response.setHeader("nju-token",JwtUtil.createToken(userId2,userType2,shortTokenTime));
+                response.setHeader("nju-long-token",JwtUtil.createToken(userId2,userType2,longTokenTime));
             }
         }
         //对长期token进行验证
